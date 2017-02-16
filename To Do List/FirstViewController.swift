b65b3ce1b8a457b857d2e556e3fb46c857bf3ec3
Marc Aupont <marc@digimarktech.com>
2017-02-16 17:13:25 -0500

    Completed To DO List app on ios9.3

--- To Do List/FirstViewController.swift
+++ To Do List/FirstViewController.swift
@@ -8,16 +8,67 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+//This line defines the toDoList array that takes value types of "String" and is currently empty
+var toDoList = [String]()
 
+class FirstViewController: UIViewController, UITableViewDelegate {
+    
+    
+    @IBOutlet var toDoListTable: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //This will check to ensure we have data in toDoList before trying to store to Device
+        if (NSUserDefaults.standardUserDefaults().objectForKey("toDoList") != nil) {
+        
+            toDoList = NSUserDefaults.standardUserDefaults().objectForKey("toDoList") as! [String] //load saved data into array
+            
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    //This piece of code allows the number of cells to be based on the number of items in the toDoList array
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return toDoList.count
+    }
+    
+    //This piece of code writes each item to each row
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+        
+        cell.textLabel?.text = toDoList[indexPath.row]
+        
+        return cell
+        
+    }
+    
+    //This function handles the ability to swipe to the left to delete table items
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        
+        //check if user chose to delete
+        if editingStyle == UITableViewCellEditingStyle.Delete {
+            
+            toDoList.removeAtIndex(indexPath.row)//delete item from array
+            
+            NSUserDefaults.standardUserDefaults().setObject(toDoList, forKey: "toDoList")//delete item from permanent storage
+            
+            toDoListTable.reloadData()//refresh elements of array on screen
+            
+        }
+        
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        
+        toDoListTable.reloadData() 
     }
 
 
--- To Do List/SecondViewController.swift
+++ To Do List/SecondViewController.swift
@@ -10,6 +10,27 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet var userInput: UITextField!
+    
+    @IBAction func addItemButton(sender: AnyObject) {
+        
+        if userInput.text != "" {
+        
+            //This entry adds whatever the user inputs into the toDoList array
+            toDoList.append(userInput.text!)
+            
+            //This line clears the entry so the user can add more items
+            userInput.text = ""
+            
+            //This writes the entries into permanent storage
+            NSUserDefaults.standardUserDefaults().setObject(toDoList, forKey: "toDoList")
+            
+        }
+        
+        
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +39,19 @@ class SecondViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    //This is the code that allows user to tap outside of the keyboard to close the keyboard
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(textField: UITextField!) -> Bool {
+        
+        userInput.resignFirstResponder()
+        
+        return true 
     }
 
 
